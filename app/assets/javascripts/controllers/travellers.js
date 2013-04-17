f08e0f2aa8836085dbc58c1a7dd6b8c2e4f4c8f2
jesalg <jesal7@gmail.com>
2013-04-16 18:09:44 -0700

    Ability to delete travelers and trips

--- app/assets/javascripts/controllers/travellers.js
+++ app/assets/javascripts/controllers/travellers.js
@@ -1,4 +1,4 @@
-function TravellersCtrl($scope, $location, $routeParams, Travellers, Trip) {"use strict";
+function TravellersCtrl($scope, $location, $routeParams, $dialog, Travellers, Traveller, Trip) {"use strict";
     $scope.trip = Trip.show({trip_id : $routeParams.trip_id});
 
     $scope.create = function(traveller) {
@@ -6,8 +6,30 @@ function TravellersCtrl($scope, $location, $routeParams, Travellers, Trip) {"use
         var travellerService = new Travellers(traveller);
         travellerService.$create(function(traveller) {
             $scope.trip = Trip.show({trip_id : $routeParams.trip_id});
+            //$scope.addTravellerForm.reset();
         });
-    }
+    };
+
+    $scope.remove = function(id) {
+        var title = 'Delete Traveller?', msg = 'Are you sure you want to delete this traveller?', btns = [{
+            result : 'cancel',
+            label : 'Cancel'
+        }, {
+            result : 'ok',
+            label : 'OK',
+            cssClass : 'btn-primary'
+        }];
+
+        $dialog.messageBox(title, msg, btns).open().then(function(result) {
+            if (result === 'ok') {
+                Traveller.destroy({
+                    traveller_id : id
+                }, function() {
+                    $scope.trip = Trip.show({trip_id : $routeParams.trip_id});
+                });
+            }
+        });
+    };
 }
 
 
