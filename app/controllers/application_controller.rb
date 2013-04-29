f47d8d7e85b2134b3743dec7ee659bcfe3edb4ec
jesalg <jesal7@gmail.com>
2013-04-29 13:57:04 -0700

    Installing devise and setting up app for login/registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :intercept_html_requests
-  private
-  def intercept_html_requests
-    render('home/index') if request.format == Mime::HTML
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -54,8 +54,12 @@ Travelizer::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-  root :to => 'home#index'
-
+  authenticated :user do
+    root :to => 'home#index'
+  end
+  root :to => "home#index"
+  devise_for :users
+  resources :users
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
