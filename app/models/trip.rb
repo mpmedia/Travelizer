70338e2c29dab02f8e835b3b46f0d5de3cbc5edd
jesalg <jesal7@gmail.com>
2013-06-13 11:36:08 -0700

    Adding start/end dates to the trip

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,6 +3,8 @@ class Trip
 
   field :uuid, type: String
   field :name, type: String
+  field :start, type: Date
+  field :end, type: Date
   field :description, type: String
 
   embeds_many :flights
