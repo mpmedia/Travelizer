ff285aca4951c936c7609842e8c768329a1ad11a
jesalg <jesal7@gmail.com>
2013-06-13 18:51:28 -0700

    Adding factual API stub

---
+++ app/controllers/factual_controller.rb
@@ -0,0 +1,10 @@
+class FlightController < ApplicationController
+  respond_to :json
+  factual = Factual.new("rFz3zPCHEwY6XqI3wRJCSMANI0TI4MW5Pl4dBKyj", "jrnBkDhczxgPDBOzsFKrfdHD65OIEhGVwoj7AoBl")
+
+  def index
+
+  end
+
+
+end
