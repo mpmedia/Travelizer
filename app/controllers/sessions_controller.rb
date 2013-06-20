9a9b9bbb0df5ce231e67278f0c92554d70bec7e7
jesalg <jesal7@gmail.com>
2013-06-20 16:23:18 -0700

    Require active support

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < Devise::SessionsController
-  respond_to :json, :html
+  respond_to :json
   def create
     resource = warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#failure")
     render :status => 200,
