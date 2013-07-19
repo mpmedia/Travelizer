b6e9689d24d56ac5b54e8f917a1ac6c34a730863
jesalg <jesal7@gmail.com>
2013-07-19 09:46:54 -0700

    Show google places along the chosen route

--- shared/js/app.js
+++ shared/js/app.js
@@ -54,6 +54,7 @@ function HomeCtrl($scope, $location, $http, $document, $window) {"use strict";
 	
 	$scope.drawBoxes = function(boxes) {
 		$scope.boxpolys = new Array(boxes.length);
+		$scope.service = new google.maps.places.PlacesService($scope.map);
 		for (var i = 0; i < boxes.length; i++) {
 			$scope.boxpolys[i] = new google.maps.Rectangle({
 			  bounds: boxes[i],
@@ -63,7 +64,62 @@ function HomeCtrl($scope, $location, $http, $document, $window) {"use strict";
 			  strokeWeight: 1,
 			  map: $scope.map
 			});
+			//var bounds = new google.maps.LatLngBounds(new google.maps.LatLng(-33.8902, 151.1759), new google.maps.LatLng(-33.8474, 151.2631));
+			var request = {
+				bounds: $scope.boxpolys[i].getBounds(), //The bounds within which to search for Places
+				types: ['establishment']
+			};
+			$scope.searchNearby(request, i);
 		}		
+	};
+	
+	$scope.searchNearby = function(request, timeout) {
+		setTimeout(function() { 
+			$scope.service.nearbySearch(request, function(results, status, pagination) {
+				if (status === google.maps.places.PlacesServiceStatus.OK) {
+					$scope.createMarkers(results);
+				} else {
+					console.log(status);
+				}});
+		}, (timeout * 1000));
+    };
+	
+	$scope.createMarkers = function(places) {
+		var bounds = new google.maps.LatLngBounds();
+		var markers = [];
+
+		for (var i = 0, place; place = places[i]; i++) {
+			var image = {
+				url: place.icon,
+				size: new google.maps.Size(71, 71),
+				origin: new google.maps.Point(0, 0),
+				anchor: new google.maps.Point(17, 34),
+				scaledSize: new google.maps.Size(25, 25)
+			};
+
+			var marker = new google.maps.Marker({
+				map: $scope.map,
+				icon: image, //place.photos[0].getUrl({'maxWidth': 35, 'maxHeight': 35})
+				title: place.name,
+				position: place.geometry.location
+			});
+
+			marker.set('name', place.name);
+
+			google.maps.event.addListener(marker, 'click', function(){
+				var name = this.get('name');
+				$scope.infowindow.setContent(name);
+				$scope.infowindow.open($scope.map, this);
+			});
+
+			if(place.photos && place.photos.length > 0) {
+				place.thumb = place.photos[0].getUrl({'maxWidth': 64, 'maxHeight': 64});
+			}
+			
+			bounds.extend(place.geometry.location);
+		}
+
+		$scope.map.fitBounds(bounds);
 	};
 	
 	$scope.clearBoxes = function() {
